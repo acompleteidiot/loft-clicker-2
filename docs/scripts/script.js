24d826caa026f124e8e1afc442dc42e62bc2eca5
ARandomIdiotMexican <86502822+acompleteidiot@users.noreply.github.com>
2026-09-08 16:17:24 -0600

    changed the bird image

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -17,7 +17,7 @@ const titleKeyMapHeaderStyle = {
   woft: "Woft Clicker 2",
   announcer: "Announcersoft Clicker 2",
   soft: "Soft Clicker 2",
-  bird: "Willow Flycatcher Clicker 2",
+  bird: "Least Sandpiper Clicker 2",
 };
 
 function reset() {
